287f111bfcd51fa1f41336e97dca0e8b073fd816
Keilerb <63890068+Keilerb@users.noreply.github.com>
2020-04-17 21:04:38 -0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -114,6 +114,8 @@ var l_musicDisplay = true;
 // (You can also use external URL for the ogg files)
 var l_musicPlaylist = [
 	{ogg: "animalcrossing.ogg", name: "Animals Crossing New Horizons - Main Theme"},
+	{ogg: "ffxv.ogg", name: "Final Fantasy XV - Galdin Quay"},
+	{ogg: "undertale", name: "Undertale - Home"},
 ];
 
 // Random music order?
