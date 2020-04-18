4a54fbd01a09a9e1f9d74a74acfd9eec2ae5ab96
Keilerb <63890068+Keilerb@users.noreply.github.com>
2020-04-17 20:52:23 -0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -131,8 +131,14 @@ var l_messagesEnabled = true;
 // Enter your custom messages below
 var l_messages = [
 	"Enjoy the server",
-	"Thanks for joining us!",
+	"Thanks for joining the server!",
 	"Use !unbox to open buy keys and crates and test your chances at getting a Permanent Rare Knife!",
+	"Black Market Dealers can sell exclusive, rare and powerful guns that the Gun Dealer can't.",
+	"To call an admin, type @admin and then your message to the admins.",
+	"Our Unbox System has multiple tiers of crates and keys that you can use to unbox guns, money and permanent knives"
+	"Thanks for your patience, you'll be ingame soon."
+	"Use the /advert command for class based actions."
+	"Maximize your profits. Persue the career of your choice, keep the peace, cook meth, grow weed and even make lean... the possibilites are endless."
 ];
 
 // Random message order?
